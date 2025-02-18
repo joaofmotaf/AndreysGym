61d7c8ed7b698dad92327067e86ac9b3754f017e
João <jfmf@aluno.ifnmg.edu.br>
2025-02-18 10:09:51 -0300

    Criação do Repository principal e do UsuarioRepository.

---
+++ Repositories/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AndreysGym.Entidades;
+
+namespace AndreysGym.Repositories
+{
+    public class Repository : DbContext
+    {
+        private static readonly String _connectionParams =
+            @"server=127.0.0.1;uid=root;pwd=;database=AndreysGym;";
+        public Repository() => Database.EnsureCreated();
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+        public DbSet<Plano> Planos { get; set; }
+        public DbSet<Programacao> Programacoes { get; set; }
+        public DbSet<Frequencia> Frequencias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySQL(_connectionParams);
+        }
+    }
+}
---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AndreysGym.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace AndreysGym.Repositories
+{
+    public class UsuarioRepository
+    {
+        public static void Save(Usuario usuario)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    if (usuario.Id == 0)
+                    {
+                        dbContext.Usuarios.Add(usuario);
+                    }
+                    else
+                    {
+                        dbContext.Entry(usuario).State
+                            = EntityState.Modified;
+                    }
+
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static List<Usuario> FindAll()
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    return dbContext.Usuarios.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Usuario FindById(Int64 id)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    return dbContext.Usuarios.Find(id);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Usuario FindByIdWCredencial(UInt64 id)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    return dbContext.Usuarios
+                        .Include("Credencial")
+                        .Where(u => u.Id == id)
+                        .FirstOrDefault<Usuario>();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static List<Usuario> FindByPartialName(String partialName)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    return dbContext.Usuarios
+                        .Where(u => u.Nome.Contains(partialName))
+                        .ToList<Usuario>();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void Remove(Usuario usuario)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    dbContext.Usuarios.Attach(usuario);
+                    dbContext.Usuarios.Remove(usuario);
+
+                    // OR
+
+                    // But cascade delete fails...
+                    //dbContext.Entry(usuario).State
+                    //    = EntityState.Deleted;
+
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
