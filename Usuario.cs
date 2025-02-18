d7d58dbfdd88ecd95778fdf8e165c005990fea33
joaofmotaf <joaofmota2007@gmail.com>
2025-02-17 21:39:52 -0300

    Criação da classe Usuario

---
+++ Usuario.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreysGym
+{
+    public class Usuario
+    {
+        public String Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public UInt64 Cpf { get; set; }
+        public Byte CalcularIdade()
+        {
+            return 0;
+        }
+    }
+}
