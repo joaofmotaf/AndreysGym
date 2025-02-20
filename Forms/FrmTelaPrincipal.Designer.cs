35d652f04e6fe75bed77b7dd8471e6eb6eccb2b3
Caio Souza Silva <css24@aluno.ifnmg.edu.br>
2025-02-20 09:03:56 -0300

    frmTelaPrincipal Atualizada

--- Forms/FrmTelaPrincipal.Designer.cs
+++ Forms/FrmTelaPrincipal.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mnufrmTelaPrincipal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.áreaDoAdministradorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerenciamentoDeUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.áreaDoClienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frequênciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treinosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaliaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pagamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.termosDeUsoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnufrmTelaPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnufrmTelaPrincipal
+            // 
+            this.mnufrmTelaPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.áreaDoAdministradorToolStripMenuItem,
+            this.áreaDoClienteToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.mnufrmTelaPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnufrmTelaPrincipal.Name = "mnufrmTelaPrincipal";
+            this.mnufrmTelaPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.mnufrmTelaPrincipal.TabIndex = 1;
+            this.mnufrmTelaPrincipal.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "&Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Image = global::AndreysGym.Properties.Resources.free_exit_icon_2860_thumb;
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // áreaDoAdministradorToolStripMenuItem
+            // 
+            this.áreaDoAdministradorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarToolStripMenuItem,
+            this.gerenciamentoDeUsuariosToolStripMenuItem});
+            this.áreaDoAdministradorToolStripMenuItem.Name = "áreaDoAdministradorToolStripMenuItem";
+            this.áreaDoAdministradorToolStripMenuItem.Size = new System.Drawing.Size(139, 20);
+            this.áreaDoAdministradorToolStripMenuItem.Text = "&Área do Administrador";
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.cadastrarToolStripMenuItem.Text = "Cadastrar usuário";
+            // 
+            // gerenciamentoDeUsuariosToolStripMenuItem
+            // 
+            this.gerenciamentoDeUsuariosToolStripMenuItem.Name = "gerenciamentoDeUsuariosToolStripMenuItem";
+            this.gerenciamentoDeUsuariosToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.gerenciamentoDeUsuariosToolStripMenuItem.Text = "Gerenciamento de Usuarios";
+            // 
+            // áreaDoClienteToolStripMenuItem
+            // 
+            this.áreaDoClienteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.perfilToolStripMenuItem,
+            this.frequênciaToolStripMenuItem,
+            this.treinosToolStripMenuItem,
+            this.avaliaçõesToolStripMenuItem,
+            this.pagamentosToolStripMenuItem});
+            this.áreaDoClienteToolStripMenuItem.Name = "áreaDoClienteToolStripMenuItem";
+            this.áreaDoClienteToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.áreaDoClienteToolStripMenuItem.Text = "&Área do Cliente";
+            // 
+            // perfilToolStripMenuItem
+            // 
+            this.perfilToolStripMenuItem.Name = "perfilToolStripMenuItem";
+            this.perfilToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.perfilToolStripMenuItem.Text = "Perfil";
+            // 
+            // frequênciaToolStripMenuItem
+            // 
+            this.frequênciaToolStripMenuItem.Name = "frequênciaToolStripMenuItem";
+            this.frequênciaToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.frequênciaToolStripMenuItem.Text = "Frequência";
+            // 
+            // treinosToolStripMenuItem
+            // 
+            this.treinosToolStripMenuItem.Name = "treinosToolStripMenuItem";
+            this.treinosToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.treinosToolStripMenuItem.Text = "Treinos";
+            // 
+            // avaliaçõesToolStripMenuItem
+            // 
+            this.avaliaçõesToolStripMenuItem.Name = "avaliaçõesToolStripMenuItem";
+            this.avaliaçõesToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.avaliaçõesToolStripMenuItem.Text = "Avaliações";
+            // 
+            // pagamentosToolStripMenuItem
+            // 
+            this.pagamentosToolStripMenuItem.Name = "pagamentosToolStripMenuItem";
+            this.pagamentosToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.pagamentosToolStripMenuItem.Text = "Pagamentos";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.termosDeUsoToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.ajudaToolStripMenuItem.Text = "&Ajuda";
+            // 
+            // termosDeUsoToolStripMenuItem
+            // 
+            this.termosDeUsoToolStripMenuItem.Name = "termosDeUsoToolStripMenuItem";
+            this.termosDeUsoToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.termosDeUsoToolStripMenuItem.Text = "Termos de uso";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // FrmTelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TelaPrincipal";
+            this.Controls.Add(this.mnufrmTelaPrincipal);
+            this.Name = "FrmTelaPrincipal";
+            this.Text = "Tela Principal";
+            this.mnufrmTelaPrincipal.ResumeLayout(false);
+            this.mnufrmTelaPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip mnufrmTelaPrincipal;
+        private ToolStripMenuItem arquivoToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+        private ToolStripMenuItem áreaDoAdministradorToolStripMenuItem;
+        private ToolStripMenuItem cadastrarToolStripMenuItem;
+        private ToolStripMenuItem gerenciamentoDeUsuariosToolStripMenuItem;
+        private ToolStripMenuItem áreaDoClienteToolStripMenuItem;
+        private ToolStripMenuItem perfilToolStripMenuItem;
+        private ToolStripMenuItem frequênciaToolStripMenuItem;
+        private ToolStripMenuItem treinosToolStripMenuItem;
+        private ToolStripMenuItem avaliaçõesToolStripMenuItem;
+        private ToolStripMenuItem pagamentosToolStripMenuItem;
+        private ToolStripMenuItem ajudaToolStripMenuItem;
+        private ToolStripMenuItem termosDeUsoToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
     }
 }
