be301681ae79d9e8c17d90566d4ffbb8f662f737
maalvesf <mcaf3@aluno.ifnmg.edu.br>
2025-02-23 15:29:14 -0300

    Alteração na classe Avaliacao.cs

--- Entidades/Avaliacao.cs
+++ Entidades/Avaliacao.cs
@@ -9,8 +9,8 @@ namespace AndreysGym.Entidades
 {
     public class Avaliacao
     {
-        public Usuario Usuario { get; set; }
         [Required]
+        public Usuario Usuario { get; set; }
 
         public UInt64 UsuarioId { get; set; }
 
